079d544b8c46df1fe720b45187a024ac1d178e1b
sachindra-maharjan <sachindra.maharjan@outlook.com>
2023-04-25 17:57:07 +0000

    Updated configuration

--- src/main/java/com/newyeti/common/discoveryserver/config/SecurityConfig.java
+++ src/main/java/com/newyeti/common/discoveryserver/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.newyeti.common.discoveryserver.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,6 +14,8 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
+@EnableConfigurationProperties
+@ConfigurationProperties
 @EnableWebSecurity
 public class SecurityConfig {
 
